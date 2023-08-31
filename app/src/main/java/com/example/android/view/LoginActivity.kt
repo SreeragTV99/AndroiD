2cf53593a1bad53e70f8cf303e0ff0e7b5f77c51
SreeragTV99 <sreeragtv03@gmail.com>
2023-08-31 10:52:56 +0530

    fixed issue of toolbar on login

--- app/src/main/java/com/example/android/view/LoginActivity.kt
+++ app/src/main/java/com/example/android/view/LoginActivity.kt
@@ -8,6 +8,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setSupportActionBar(findViewById(R.id.toolbar))
         supportFragmentManager.beginTransaction().replace(R.id.containerLogin,LoginFragment()).commit()
     }
 }
