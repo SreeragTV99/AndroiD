0645c23de74b5b8591b2dd978f158f00042acf8f
SreeragTV99 <sreeragtv03@gmail.com>
2023-09-01 17:20:49 +0530

    Removed unwanted TODOs

--- app/src/main/java/com/example/android/view/ProfileFragment.kt
+++ app/src/main/java/com/example/android/view/ProfileFragment.kt
@@ -13,18 +13,10 @@ import com.example.android.model.LoginRes
 import com.example.android.model.setImage
 import com.google.gson.Gson
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     lateinit var image:ImageView
@@ -34,7 +26,6 @@ class ProfileFragment : Fragment() {
     lateinit var lastName:TextView
     lateinit var gender:TextView
     lateinit var email:TextView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,15 +72,6 @@ class ProfileFragment : Fragment() {
         return userData
     }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ProfileFragment().apply {
